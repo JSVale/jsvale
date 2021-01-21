d5b255a39b0532890f3be03f226d28c2d297430d
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-01-21 00:19:38 -0300

    Position in css

--- pages/index.js
+++ pages/index.js
@@ -13,22 +13,27 @@ export default function Home(props) {
 				<link rel="icon" href="https://avatars3.githubusercontent.com/u/57258568?v=4" />
 				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
 			</Head>
-			<p>Instruções para o curso de desenvolvimento web</p>
-			<p>O curso vai acontecer nos dias 25, 26 e 27 de Janeiro de 2021</p>
-			<p>Nossos links</p>
-			<div className={styles.links}>
-				<Link href="https://discord.gg/vGH7wJN9"><a><FaDiscord size={40}/></a></Link>
-				<Link href="mailto:jsinvale@gmail.com"><a><FaAt size={40} /></a></Link>
-				<Link href="https://www.notion.so/JS-Vale-df525e12f8ae492698c2bef0dd4d8276"><a><SiNotion size={40} /></a></Link>
-			</div>
-			{props.posts.map((post, idx) => (
-				<article key={idx}>
-					<Link href={'/posts/' + post.slug}>
-						<a className={styles.linkPost}>{post.title}</a>
-					</Link>
-				</article>
-			))}
-
+			<main>
+				<p className={styles.instructions}>Instruções para o curso de desenvolvimento web</p>
+				<p className={styles.dateCourse}>O curso vai acontecer nos dias 25, 26 e 27 de Janeiro de 2021</p>
+				<section className={styles.posts}>
+					{props.posts.map((post, idx) => (
+						<article key={idx}>
+							<Link href={'/posts/' + post.slug}>
+								<a className={styles.linkPost}>{post.title}</a>
+							</Link>
+						</article>
+					))}
+				</section>
+			</main>
+			<footer className={styles.footer}>
+				<p>Nossos links</p>
+				<div className={styles.links}>
+					<Link href="https://discord.gg/vGH7wJN9"><a><FaDiscord size={40} /></a></Link>
+					<Link href="mailto:jsinvale@gmail.com"><a><FaAt size={40} /></a></Link>
+					<Link href="https://www.notion.so/JS-Vale-df525e12f8ae492698c2bef0dd4d8276"><a><SiNotion size={40} /></a></Link>
+				</div>
+			</footer>
 		</div>
 
 	)
